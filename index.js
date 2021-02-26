f93ece2c666033f8a6a62d8b422624dab79a8453
ccchippp <clempke90@gmail.com>
2021-02-26 10:16:36 -0500

    keep going

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer
+const leastFavoriteCustomer = 'def bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    var notBob = 'not ' + customerName.toLowerCase()
+    bestCustomer = notBob
+}
+
+function overwriteBestCustomer() {
+    var maybeBob = 'maybe ' + customerName.toLowerCase()
+    bestCustomer = maybeBob
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = leastFavoriteCustomer.toUpperCase()
+}
